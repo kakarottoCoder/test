a82a939ffc658728e6c10ac813101f3430bfd290
cuiaoming <cuiaoming.bytedance.com>
2022-06-04 16:22:12 +0800

    正式开发

--- handler.go
+++ handler.go
@@ -10,4 +10,5 @@ func GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	io.WriteString(w, "Get User Handler")
 	io.WriteString(w, "hello,Git！")
 	io.WriteString(w, "初始化AAABBB")
+	io.WriteString(w, "正式开发")
 }
